77ed62e16121ea4d3a931ebe7bf311d59b7fbac4
Seehait Chockthanyawat <s.chockthanyawat@gmail.com>
2017-04-29 15:58:28 +0700

    Add Node

---
+++ src/main/java/diagram/Node.java
@@ -0,0 +1,92 @@
+package diagram;
+
+import exceptions.ChildNotExistedException;
+import exceptions.ParentNotExistedException;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+/**
+ * Created by seehait on 4/29/17.
+ */
+public class Node {
+    private String label;
+    private ArrayList<Node> parents;
+    private ArrayList<Node> childs;
+    private HashMap<Node, ArrayList<Node>> siblingsFromParent;
+    private HashMap<Node, ArrayList<Node>> siblingsFromChild;
+
+    public Node(String label) {
+        this.label = label;
+        parents = new ArrayList<Node>();
+        childs = new ArrayList<Node>();
+        siblingsFromParent = new HashMap<Node, ArrayList<Node>>();
+        siblingsFromChild = new HashMap<Node, ArrayList<Node>>();
+    }
+
+    public String getLabel() {
+        return label;
+    }
+
+    public boolean addParent(Node parent) {
+        if (parents.contains(parent)) {
+            return false;
+        }
+
+        parents.add(parent);
+        siblingsFromParent.put(parent, new ArrayList<Node>());
+        return true;
+    }
+
+    public ArrayList<Node> getParents() {
+        return parents;
+    }
+
+    public boolean addChild(Node child) {
+        if (childs.contains(child)) {
+            return false;
+        }
+
+        childs.add(child);
+        siblingsFromChild.put(child, new ArrayList<Node>());
+        return true;
+    }
+
+    public ArrayList<Node> getChilds() {
+        return childs;
+    }
+
+    public boolean addSiblingsFromParent(Node parent, Node siblings) {
+        if (parents.contains(parent)) {
+            siblingsFromParent.get(parent).add(siblings);
+            return true;
+        }
+
+        return false;
+    }
+
+    public ArrayList<Node> getSiblingsFromParent(Node parent) {
+        if (parents.contains(parent)) {
+            return siblingsFromParent.get(parent);
+        }
+
+        throw new ParentNotExistedException(parent);
+    }
+
+    public boolean addSiblingsFromChild(Node child, Node siblings) {
+        if (childs.contains(child)) {
+            siblingsFromChild.get(child).add(siblings);
+            return true;
+        }
+
+        return false;
+    }
+
+    public ArrayList<Node> getSiblingsFromChild(Node child) {
+        if (childs.contains(child)) {
+            return siblingsFromChild.get(child);
+        }
+
+        throw new ChildNotExistedException(child);
+    }
+}
---
+++ src/test/java/diagram/NodeTest.java
@@ -0,0 +1,139 @@
+package diagram;
+
+import exceptions.ChildNotExistedException;
+import exceptions.ParentNotExistedException;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Created by seehait on 4/29/17.
+ */
+public class NodeTest {
+    private Node underTest;
+    private Node[] nodes;
+    private static final String LABEL = "Label";
+
+    @Before
+    public void setUp() {
+        underTest = new Node(LABEL);
+
+        nodes = new Node[5];
+        for (int i = 0; i < 5; i++) {
+            nodes[i] = new Node(LABEL);
+        }
+    }
+
+    @Test
+    public void nodeShouldHasCorrectLabel() {
+        Assert.assertEquals(LABEL, underTest.getLabel());
+    }
+
+    @Test
+    public void nodeShouldContainCorrectParent() {
+        underTest.addParent(nodes[0]);
+
+        Assert.assertEquals(nodes[0], underTest.getParents().get(0));
+    }
+
+    @Test
+    public void nodeShouldHasCorrectNumberOfParents() {
+        underTest.addParent(nodes[0]);
+        underTest.addParent(nodes[1]);
+
+        Assert.assertEquals(2, underTest.getParents().size());
+    }
+
+    @Test
+    public void nodeShouldHasUniqueParent() {
+        underTest.addParent(nodes[0]);
+        underTest.addParent(nodes[0]);
+
+        Assert.assertEquals(1, underTest.getParents().size());
+    }
+
+    @Test
+    public void nodeShouldContainCorrectChild() {
+        underTest.addChild(nodes[0]);
+
+        Assert.assertEquals(nodes[0], underTest.getChilds().get(0));
+    }
+
+    @Test
+    public void nodeShouldHasCorrectNumberOfChilds() {
+        underTest.addChild(nodes[0]);
+        underTest.addChild(nodes[1]);
+
+        Assert.assertEquals(2, underTest.getChilds().size());
+    }
+
+    @Test
+    public void setNodesShouldHasUniqueChild() {
+        underTest.addChild(nodes[0]);
+        underTest.addChild(nodes[0]);
+
+        Assert.assertEquals(1, underTest.getChilds().size());
+    }
+
+    @Test
+    public void nodeShouldBeAbleToAddSiblingsToExistedParent() {
+        underTest.addParent(nodes[0]);
+
+        Assert.assertTrue(underTest.addSiblingsFromParent(nodes[0], nodes[1]));
+    }
+
+    @Test
+    public void nodeShouldNotBeAbleToAddSiblingsToNonExistedParent() {
+        underTest.addParent(nodes[0]);
+
+        Assert.assertFalse(underTest.addSiblingsFromParent(nodes[1], nodes[2]));
+    }
+
+    @Test
+    public void nodeShouldReturnCorrectSiblingsFromParent() {
+        underTest.addParent(nodes[0]);
+        underTest.addSiblingsFromParent(nodes[0], nodes[1]);
+
+        underTest.addParent(nodes[2]);
+        underTest.addSiblingsFromParent(nodes[2], nodes[3]);
+
+        Assert.assertEquals(nodes[1], underTest.getSiblingsFromParent(nodes[0]).get(0));
+        Assert.assertEquals(nodes[3], underTest.getSiblingsFromParent(nodes[2]).get(0));
+    }
+
+    @Test(expected = ParentNotExistedException.class)
+    public void nodeShouldThrowExceptionWhenGetSiblingsFromNotExistedParent() {
+        underTest.getSiblingsFromParent(nodes[0]);
+    }
+
+    @Test
+    public void nodeShouldBeAbleToAddSiblingsToExistedChild() {
+        underTest.addChild(nodes[0]);
+
+        Assert.assertTrue(underTest.addSiblingsFromChild(nodes[0], nodes[1]));
+    }
+
+    @Test
+    public void nodeShouldNotBeAbleToAddSiblingsToNonExistedChild() {
+        underTest.addChild(nodes[0]);
+
+        Assert.assertFalse(underTest.addSiblingsFromChild(nodes[1], nodes[2]));
+    }
+
+    @Test
+    public void nodeShouldReturnCorrectSiblingsFromChild() {
+        underTest.addChild(nodes[0]);
+        underTest.addSiblingsFromChild(nodes[0], nodes[1]);
+
+        underTest.addChild(nodes[2]);
+        underTest.addSiblingsFromChild(nodes[2], nodes[3]);
+
+        Assert.assertEquals(nodes[1], underTest.getSiblingsFromChild(nodes[0]).get(0));
+        Assert.assertEquals(nodes[3], underTest.getSiblingsFromChild(nodes[2]).get(0));
+    }
+
+    @Test(expected = ChildNotExistedException.class)
+    public void nodeShouldThrowExceptionWhenGetSiblingsFromNotExistedChild() {
+        underTest.getSiblingsFromChild(nodes[0]);
+    }
+}
